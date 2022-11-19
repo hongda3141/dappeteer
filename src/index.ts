1830c9143f03097d84bf66eab6717ae3544de06e
cryptape.michael <default@email.com>
2022-11-19 23:05:23 +0800

    remove all

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// re-export
-export { getMetamask, getMetamaskWindow } from './metamask';
-export * from './types';
-export * from './setup';
-export { DappateerJestConfig } from './jest/global';
-
-// default constants
-export const RECOMMENDED_METAMASK_VERSION = 'v10.15.0';
